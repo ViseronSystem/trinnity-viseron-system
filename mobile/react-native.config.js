cf60d5bd74ad1403d317e9f703b137f3f39200b2
ViseronSystem <viseron@trinnity.ai>
2026-07-31 01:57:25 +0200

    Add react-native.config.js with android.packageName for autolinking

---
+++ mobile/react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    android: {
+      packageName: "com.trinnity.viseron",
+      sourceDir: "./android",
+    },
+    ios: {
+      sourceDir: "./ios",
+    },
+  },
+  assets: [],
+};
